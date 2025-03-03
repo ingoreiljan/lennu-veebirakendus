c3de0a3e6fdfaffe40594623f3daa0afa7cf4f8e
ingoreiljan <ingoreiljan@gmail.com>
2025-03-03 22:37:40 +0200

    First commit! Hello world web app.

---
+++ src/main/java/com/jetbrains/ingo/lennu_veebirakendus/LennuVeebirakendusApplication.java
@@ -0,0 +1,13 @@
+package com.jetbrains.ingo.lennu_veebirakendus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LennuVeebirakendusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LennuVeebirakendusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jetbrains/ingo/lennu_veebirakendus/LennuVeebirakendusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jetbrains.ingo.lennu_veebirakendus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LennuVeebirakendusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
