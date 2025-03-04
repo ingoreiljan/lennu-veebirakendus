bd91ccaa2b665f45b881d5eb62e4b746c34be4e6
ingoreiljan <ingoreiljan@gmail.com>
2025-03-04 22:53:58 +0200

    Merge remote-tracking branch 'origin/master'

---
+++ src/main/java/com/ingo/lennu_veebirakendus/test.java
@@ -0,0 +1,16 @@
+package com.ingo.lennu_veebirakendus;
+
+public class test {
+
+    public static void main(String[] args) {
+        System.out.println(fileReader.readFile()
+        );
+
+        String[] a = {String.valueOf(fileReader.readFile())};
+        System.out.println(a[0]);
+
+
+
+
+    }
+}
